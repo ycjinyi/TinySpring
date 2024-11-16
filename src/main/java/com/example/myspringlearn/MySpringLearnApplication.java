260380cd4c37bbd7d856f8897619269b59be0ef7
ycjinyi <liujinyicn@foxmail.com>
2024-11-16 16:09:12 +0800

    add start file

---
+++ src/main/java/com/example/myspringlearn/MySpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspringlearn/MySpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
